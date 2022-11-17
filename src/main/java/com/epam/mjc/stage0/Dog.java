f01104c173b337b2a65a045624aac784d881c8e5
Shodmon-creator <mamayusupovshodmon2003@gmail.com>
2022-11-17 10:06:31 +0500

    one unnecessary override function have been deleted

--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -3,9 +3,4 @@ package com.epam.mjc.stage0;
 public class Dog extends Animal {
     public Dog(){super("brown",4,true);
      }
-
-    @Override
-    public String getDescription() {
-        return super.getDescription();
-    }
 }
